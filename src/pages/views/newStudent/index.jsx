19e1823d959f5b1a1f96aab3cf32c74001597660
codewithjohnson <75730906+codewithjohnson@users.noreply.github.com>
2022-12-27 10:10:14 +0100

    adjusted submenu hover color

--- src/pages/views/newStudent/index.jsx
+++ src/pages/views/newStudent/index.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NewStudent = () => {
+  // const test = ["home","students", "add-student"];
+  // const path = test.reduce((acc, curr) => {
+  //   const path = acc.length ? `/${acc[acc.length - 1]}/${curr}` : curr;
+  //   return [...acc, path];
+  // }, []);
+
+ 
+
   return (
     <div className="w-full min-h-screen bg-white new rounded-2xl">
-      <div className="p-5 border-b border-b-slate-200">
+      <div className="flex items-center justify-between p-5 border-b border-b-slate-200">
         <h2 className="text-lg capitalize select-none new--student font-poppins">
           new student
         </h2>
+
+        <div className="pathTrack">
+          {/* TODOS: create relative path */}
+        </div>
       </div>
       <div className="newStudentForm">
         
