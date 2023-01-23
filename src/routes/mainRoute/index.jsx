d22709be73959940f1a8d567468cb8388a914cd6
Ishola Deborah <isholadeborahope@gmail.com>
2023-01-23 18:30:06 +0100

    errorpage

--- src/routes/mainRoute/index.jsx
+++ src/routes/mainRoute/index.jsx
@@ -1,12 +1,13 @@
 import { lazy } from "react";
 import Loadable from "../../components/loadable/";
 import { studentRoute } from "../studentRoute";
-
+import ErrorPage from "../../pages/views/errorPage/ErrorPage";
 const App = Loadable(lazy(() => import("../../App")));
 const Dashboard = Loadable(lazy(() => import("../../pages/views/dashboard")));
 export const mainRoute = {
   path: "/",
   element: <App />,
+  errorElement: <ErrorPage />,
   children: [
     {
       path: "/",
