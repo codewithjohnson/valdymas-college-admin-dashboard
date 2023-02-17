6568651184c0a0b1768af3ec637e1951c6a08cef
codewithjohnson <75730906+codewithjohnson@users.noreply.github.com>
2023-02-17 11:17:37 +0100

    commit

--- src/services/firebase.config.jsx
+++ src/services/firebase.config.jsx
@@ -1,10 +1,10 @@
 // Firebase configuration
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyD2R8An54t5vzPIViWJ2zi2mgRCJEOqLfI",
-  authDomain: "valdymas-admin-dashboard.firebaseapp.com",
-  projectId: "valdymas-admin-dashboard",
-  storageBucket: "valdymas-admin-dashboard.appspot.com",
-  messagingSenderId: "459954975029",
-  appId: "1:459954975029:web:bc3413ffbb339291435c7c",
+  apiKey: "AIzaSyBY9mJcCIIK3e8haJgstVrX_XjZ0QyrsC8",
+  authDomain: "valdymas-admin-dashboard-8ef0d.firebaseapp.com",
+  projectId: "valdymas-admin-dashboard-8ef0d",
+  storageBucket: "valdymas-admin-dashboard-8ef0d.appspot.com",
+  messagingSenderId: "253665808813",
+  appId: "1:253665808813:web:1d3d3270444273e5ac1124",
 };
