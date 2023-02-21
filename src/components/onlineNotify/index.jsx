ddfc580d3e191cb4e49b44a815abffa0404f77ed
codewithjohnson <75730906+codewithjohnson@users.noreply.github.com>
2023-02-21 09:27:03 +0100

    commit

--- src/components/onlineNotify/index.jsx
+++ src/components/onlineNotify/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const OnlineViewer = () => {
   return (
-    <div className="fixed top-0 right-0 left-0 z-[1500] w-full bg-red-500 font-semibold   text-white flex flex-row justify-center items-center">
+    <div className="fixed top-0 right-0 left-0 z-[1500] w-full bg-teal-500 font-thin text-black flex flex-row justify-center items-center">
       oops! you are offline, please check your internet connection
     </div>
   );
--- src/utilities/menuItems.jsx
+++ src/utilities/menuItems.jsx
@@ -2,36 +2,56 @@ export const sideBarMenuItems = [
   {
     id: 1,
     label: "dashboard",
-    defaultOpen: true,
+    defaultOpen: false,
     icon: (
-      <span className="material-symbols-outlined submenuIcon">dashboard_customize</span>
+      <span className="material-symbols-outlined submenuIcon">
+        dashboard_customize
+      </span>
     ),
     subMenus: [
       { id: 1, label: "default", link: "/dashboard" },
       { id: 2, label: "analytics", link: "/analytics" },
     ],
   },
+
   {
     id: 2,
+    label: "admins",
+    defaultOpen: true,
+    icon: (
+      <span className="material-symbols-outlined submenuIcon">
+        admin_panel_settings
+      </span>
+    ),
+    subMenus: [
+      { id: 1, label: "admins", link: "/admins" },
+      { id: 1, label: "add new", link: "/admins/add-admin" },
+    ],
+  },
+  {
+    id: 3,
     label: "students",
     defaultOpen: true,
     icon: <span className="material-symbols-outlined submenuIcon">group</span>,
     subMenus: [
-      { id: 1, label: "all students", link: "/students" },
+      { id: 1, label: " students", link: "/students" },
       { id: 2, label: "add student", link: "students/add-student/biodata" },
       { id: 3, label: "results", link: "/" },
     ],
   },
   {
-    id: 3,
+    id: 4,
     label: "departments",
     defaultOpen: true,
     icon: <span className="material-symbols-outlined submenuIcon">school</span>,
     subMenus: [
       { id: 1, label: "sciences", link: "/students/departments/sciences" },
-      { id: 2, label: "social sciences", link: "/students/departments/social-sciences" },
+      {
+        id: 2,
+        label: "social sciences",
+        link: "/students/departments/social-sciences",
+      },
       { id: 3, label: "art", link: "/students/departments/arts" },
     ],
   },
-  
 ];
