1e17c441f28c8cc2dbb60ac5ef97d2bc28b65d89
muyiwa johnson <75730906+codewithjohnson@users.noreply.github.com>
2023-06-28 05:12:22 +0100

    updated error page

--- src/pages/errorPage/Errorpage.jsx
+++ src/pages/errorPage/Errorpage.jsx
@@ -4,19 +4,19 @@ import { Link } from "react-router-dom";
 const ErrorPage = () => {
   return (
     <div>
-      <div class="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col lg:flex-row md:gap-26 gap-14">
-        <div class="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
-          <div class="relative">
-            <div class="absolute">
-              <div class="">
-                <div class="text-[50px] font-dark font-bold">
-                  <h1 class="text-[50px] font-dark font-bold">404</h1>
+      <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col lg:flex-row md:gap-26 gap-14">
+        <div className="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
+          <div className="relative">
+            <div className="absolute">
+              <div className="">
+                <div className="text-[50px] font-dark font-bold">
+                  <h1 className="text-[50px] font-dark font-bold">404</h1>
                 </div>
-                <p class="my-2 text-gray-800">
+                <p className="my-2 text-gray-800">
                   Sorry about that! Please visit our homepage to get where you
                   need to go.
                 </p>
-                <button class=" lg:w-auto my-2 border sm:text-center rounded md py-4 px-8 text-center bg-indigo-500 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">
+                <button className=" lg:w-auto my-2 border sm:text-center rounded md py-4 px-8 text-center bg-indigo-500 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">
                    <Link to="/">Back To Homepage</Link>
                 </button>
               </div>
